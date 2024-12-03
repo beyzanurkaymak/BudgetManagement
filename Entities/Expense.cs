a7c898f85e59fcbcfd78e7b95e8c08f526d799a6
busraknar <b.ertas91@gmail.com>
2024-12-03 12:53:43 +0300

    Entity Layer is created

---
+++ Entities/Expense.cs
@@ -0,0 +1,11 @@
+﻿namespace BudgetManagement.Entities
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public decimal Amount { get; set; }
+        public string? Category { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
